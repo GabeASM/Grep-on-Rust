f645b23dedc8043f7f474c69ad8f929c3227a57a
Gabriel ASM <g.aillapan01@ufromail.cl>
2022-06-29 16:01:59 -0400

    Commit inicial con Cargo Rust

---
+++ Grep/target/rls/debug/deps/Grep-2e6a9bb0bdf24625.d
@@ -0,0 +1,5 @@
+c:\Users\Gabriel\Desktop\RustApp\Grep-on-Rust\Grep\target\rls\debug\deps\Grep-2e6a9bb0bdf24625.rmeta: src\main.rs
+
+c:\Users\Gabriel\Desktop\RustApp\Grep-on-Rust\Grep\target\rls\debug\deps\Grep-2e6a9bb0bdf24625.d: src\main.rs
+
+src\main.rs:
---
+++ Grep/target/rls/debug/deps/Grep-e875874bb922dc40.d
@@ -0,0 +1,5 @@
+c:\Users\Gabriel\Desktop\RustApp\Grep-on-Rust\Grep\target\rls\debug\deps\Grep-e875874bb922dc40.rmeta: src\main.rs
+
+c:\Users\Gabriel\Desktop\RustApp\Grep-on-Rust\Grep\target\rls\debug\deps\Grep-e875874bb922dc40.d: src\main.rs
+
+src\main.rs:

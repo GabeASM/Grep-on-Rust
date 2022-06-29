4121153aec3f3cf3f1717ba549f519f3777b3361
D4RK57 <danielruizk@gmail.com>
2022-06-29 16:08:03 -0400

    saludo

---
+++ Grep/target/debug/deps/Grep-2e6a9bb0bdf24625.d
@@ -0,0 +1,5 @@
+c:\Users\Usuario\Desktop\RustProyect\Grep-on-Rust\Grep\target\debug\deps\Grep-2e6a9bb0bdf24625.rmeta: src\main.rs
+
+c:\Users\Usuario\Desktop\RustProyect\Grep-on-Rust\Grep\target\debug\deps\Grep-2e6a9bb0bdf24625.d: src\main.rs
+
+src\main.rs:
---
+++ Grep/target/debug/deps/Grep-e875874bb922dc40.d
@@ -0,0 +1,5 @@
+c:\Users\Usuario\Desktop\RustProyect\Grep-on-Rust\Grep\target\debug\deps\Grep-e875874bb922dc40.rmeta: src\main.rs
+
+c:\Users\Usuario\Desktop\RustProyect\Grep-on-Rust\Grep\target\debug\deps\Grep-e875874bb922dc40.d: src\main.rs
+
+src\main.rs:

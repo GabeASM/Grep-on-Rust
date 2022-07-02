241eba8efbeeb7a9e2de7f3ee5deee506c766991
Gabriel ASM <g.aillapan01@ufromail.cl>
2022-07-02 14:58:48 -0400

    poema agregado

---
+++ Grep/target/debug/Grep.d
@@ -0,0 +1,1 @@
+C:\Users\Gabriel\Desktop\Nueva\ carpeta\ (3)\Grep-on-Rust\Grep\target\debug\Grep.exe: C:\Users\Gabriel\Desktop\Nueva\ carpeta\ (3)\Grep-on-Rust\Grep\src\main.rs
---
+++ Grep/target/debug/deps/Grep.d
@@ -0,0 +1,5 @@
+C:\Users\Gabriel\Desktop\Nueva carpeta (3)\Grep-on-Rust\Grep\target\debug\deps\Grep.exe: src\main.rs
+
+C:\Users\Gabriel\Desktop\Nueva carpeta (3)\Grep-on-Rust\Grep\target\debug\deps\Grep.d: src\main.rs
+
+src\main.rs:
